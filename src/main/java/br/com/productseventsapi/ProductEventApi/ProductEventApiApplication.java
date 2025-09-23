776ec08053ef5b48275cb999bcddf54bea2e03a9
rian-develp <riandeveloper1@gmail.com>
2025-09-22 21:32:28 -0300

    Subindo versão inicial do projeto

---
+++ src/main/java/br/com/productseventsapi/ProductEventApi/ProductEventApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.productseventsapi.ProductEventApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductEventApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductEventApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/productseventsapi/ProductEventApi/ProductEventApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.productseventsapi.ProductEventApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductEventApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
