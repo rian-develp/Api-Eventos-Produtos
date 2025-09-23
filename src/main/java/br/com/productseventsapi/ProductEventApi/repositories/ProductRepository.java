7e77d33bbee7ee91aa0ddfc08c023fa0baf98387
rian-develp <riandeveloper1@gmail.com>
2025-09-23 19:53:05 -0300

    Criado os respositórios de cada entidade

---
+++ src/main/java/br/com/productseventsapi/ProductEventApi/repositories/ProductRepository.java
@@ -0,0 +1,18 @@
+package br.com.productseventsapi.ProductEventApi.repositories;
+
+import br.com.productseventsapi.ProductEventApi.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM products WHERE id = :id", nativeQuery = true)
+    void deleteProductById(@Param("id") Integer id);
+}
